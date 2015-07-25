4a047b5bb01166ec8b8a07b7b24ff88f1c16fdf3
Omar <alhait.omar@gmail.com>
2015-07-24 23:07:52 -0700

    Revert "Revert "reverting""
    This reverts commit 891aba2996491089f7e5e557e6e3d8b222107573.

--- js/custom.js
+++ js/custom.js
@@ -13,13 +13,28 @@
 // }
 $(function() {
   var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
+  var clicked = false;
   $('img.circle').on('mouseenter', function() {
-    $(this).addClass('animated pulse1').on('mouseleave', function() {
-      $(this).removeClass('animated pulse1');
-      $(this).addClass('animated pulse2').one(animationEnd,function() {
-        $(this).removeClass('animated pulse2');
-      });
+    $(this).addClass('animated pulse1');
+    $(this).on('mousedown', function() {
+      clicked = true;
+      console.log("true");
     });
+    if(clicked == false) {
+      $(this).on('mouseleave', function() {
+        $(this).removeClass('animated pulse1');
+        $(this).addClass('animated pulse2').one(animationEnd,function() {
+          $(this).removeClass('animated pulse2');
+        });
+
+      });
+    }
+    else {
+      $(this).on('mouseleave', function() {
+          $(this).removeClass('animated pulse1');
+          clicked = false;
+          console.log("false again");
+        });
+    }
   });
 });
