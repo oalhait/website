8495542283a8e09c9c036865b57fd1947d8b8192
Omar <alhait.omar@gmail.com>
2015-10-27 02:24:37 -0400

    issue with images that needs taking care of

--- js/init.js
+++ js/init.js
@@ -1,3 +1,9 @@
+// /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'file://js/particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
+
+
 (function($){
   $(function(){
 
