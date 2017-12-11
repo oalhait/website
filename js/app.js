bde1c51b66046fbb4eb6658c4de7ea232cad7f8c
Omar <alhait.omar@gmail.com>
2017-12-10 22:58:36 -0500

    animations

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
-var ref = $(".subtitle");
-$(ref).click(function() {
-  if(!$(this).hasClass("isTitle")) {
-    if($(this).hasClass("notTitle")) {
-      $(this).removeClass("notTitle");
-    }
-    $(this).addClass("isTitle");
-    $(this).siblings().fadeTo(600,0);
-  }
-  else {
-    $(this).addClass("notTitle");
-    $(this).removeClass("isTitle");
-    $(this).siblings().fadeIn();
-    $(this).siblings().fadeTo(600,100);
+$(document).on("click", "#about-button", function() {
+  $("#projects-page").hide();
+  $("#design-page").hide();
+  $("#photography-page").hide();
+  $("#inspiration-page").hide();
+  $("#about-page").hide();
+  $("#about-page").fadeIn("slow");
+});
 
-  }
+$(document).on("click", "#projects-button", function() {
+  $("#projects-page").hide();
+  $("#design-page").hide();
+  $("#photography-page").hide();
+  $("#inspiration-page").hide();
+  $("#about-page").hide();
+  $("#projects-page").fadeIn("slow");
+});
+
+$(document).on("click", "#design-button", function() {
+  $("#projects-page").hide();
+  $("#design-page").hide();
+  $("#photography-page").hide();
+  $("#inspiration-page").hide();
+  $("#about-page").hide();
+  $("#design-page").fadeIn("slow");
+});
+
+$(document).on("click", "#photography-button", function() {
+  $("#projects-page").hide();
+  $("#design-page").hide();
+  $("#photography-page").hide();
+  $("#inspiration-page").hide();
+  $("#about-page").hide();
+  $("#photography-page").fadeIn("slow");
+});
+
+$(document).on("click", "#inspiration-button", function() {
+  $("#projects-page").hide();
+  $("#design-page").hide();
+  $("#photography-page").hide();
+  $("#inspiration-page").hide();
+  $("#about-page").hide();
+  $("#inspiration-page").fadeIn("slow");
+});
+
+
+
+$(document).ready(function() {
+  // $("#about-button").trigger("click");
 });
